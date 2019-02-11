0b12e41a3af5e96b0f624b4ab1b68bc4b61f9000
IerizhenkoGalina <44175437+IerizhenkoGalina@users.noreply.github.com>
2019-02-11 15:21:06 +0200

    Create with const.js
    Variables declared with the const keyword cannot be reassigned. However, elements in an array declared with const remain mutable. Meaning that we can change the contents of a const array, but cannot reassign a new array or a different value.

---
+++ with const.js
@@ -0,0 +1,5 @@
+const utensils = ['Fork', 'Knife', 'Chopsticks', 'Spork', 'Fall'];
+utensils [3] = 'Spoon';
+utensils [4] = 'Hot';
+console.log(utensils);
+//Prints: [ 'Fork', 'Knife', 'Chopsticks', 'Spoon', 'Hot' ]
