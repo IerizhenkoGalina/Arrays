c6aae1a7c8e1177378f5f7d3a174e96baa0830c5
IerizhenkoGalina <44175437+IerizhenkoGalina@users.noreply.github.com>
2019-02-11 15:18:30 +0200

    Create with let.js
    Variables declared with let can be reassigned, you can mutate elements in the arrays.

---
+++ with let.js
@@ -0,0 +1,5 @@
+let condiments = ['Ketchup', 'Mustard', 'Soy Sauce', 'Sriracha', 'Fall'];
+condiments [0] = 'Mayo';
+condiments [4] = ('Cool');
+console.log(condiments);
+//Prints: [ 'Mayo', 'Mustard', 'Soy Sauce', 'Sriracha', 'Cool' ]
