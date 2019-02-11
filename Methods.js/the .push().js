10f97772ebe60b64e48f58ec23f690c9411b5338
IerizhenkoGalina <44175437+IerizhenkoGalina@users.noreply.github.com>
2019-02-11 15:56:28 +0200

    Create the .push().js
    The method .push() allows us to add items to the end of an array.

---
+++ Methods.js/the .push().js
@@ -0,0 +1,29 @@
+const shopClothes = ['socks', 'jeans', 'suit',
+'jacket', 'trousers','shirt', 'tie','top',
+'skirt','sweater','shorts','dress','T-shirt',
+'coat','leggins', 'scart'];
+shopClothes.push('mittens', 'vest', 'slippers', 'cap');
+console.log(shopClothes);
+//Prints: 
+/*
+[ 'socks',
+  'jeans',
+  'suit',
+  'jacket',
+  'trousers',
+  'shirt',
+  'tie',
+  'top',
+  'skirt',
+  'sweater',
+  'shorts',
+  'dress',
+  'T-shirt',
+  'coat',
+  'leggins',
+  'scart',
+  'mittens',
+  'vest',
+  'slippers',
+  'cap' ]
+*/
