b85b76f1df262d0e7fd711b9e50b4b407181b3c5
IerizhenkoGalina <44175437+IerizhenkoGalina@users.noreply.github.com>
2019-02-11 14:09:47 +0200

    Create string.js
    Arrays are JavaScript's way of making lists. Including strings.

---
+++ list.js/string.js
@@ -0,0 +1,18 @@
+let lawsListedBelow = ['You can have a part-time job',
+'You can not buy a lottery ticket if you are under...',
+'You can buy a house',
+'You can get married with a parents permission',
+'You can rent a car',
+'You can go to a pub but you can not drink alcohol',
+'You can have a own passport'];
+console.log(lawsListedBelow);
+//Prints:
+/*
+[ 'You can have a part-time job',
+  'You can not buy a lottery ticket if you are under...',
+  'You can buy a house',
+  'You can get married with a parents permission',
+  'You can rent a car',
+  'You can go to a pub but you can not drink alcohol',
+  'You can have a own passport' ]
+  */
