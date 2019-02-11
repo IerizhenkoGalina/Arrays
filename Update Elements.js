8f54fa67118582b301d4bc5f043e4ac37fe555b3
IerizhenkoGalina <44175437+IerizhenkoGalina@users.noreply.github.com>
2019-02-11 15:06:52 +0200

    Create Update Elements.js
    We decided that we preferred to say 'rainforest' instead of 'forest'.
    The line, naturalFutures[4] = 'rainforest'; tells our program to change the item at index 4 of the naturalFutures array to be 'rainforest' instead of what is already there.

---
+++ Update Elements.js
@@ -0,0 +1,19 @@
+let naturalFutures=['canyon', 'hills', 'island',
+'lake','forest', 'ocean', 'river', 'valley',
+'mountain', 'desert', 'waterfall'];
+naturalFutures [4] = 'rainforest';
+console.log(naturalFutures);
+//Prints: 
+/*
+[ 'canyon',
+  'hills',
+  'island',
+  'lake',
+  'rainforest',
+  'ocean',
+  'river',
+  'valley',
+  'mountain',
+  'desert',
+  'waterfall' ]
+*/
