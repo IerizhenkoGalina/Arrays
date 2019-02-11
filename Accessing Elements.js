2faacd0124842500e491f707ffd7a076fa4b9326
IerizhenkoGalina <44175437+IerizhenkoGalina@users.noreply.github.com>
2019-02-11 14:42:11 +0200

    Create Accessing Elements.js
    Arrays in JavaScript are zero-indexed, meaning the positions start counting from 0.

---
+++ Accessing Elements.js
@@ -0,0 +1,13 @@
+const famousSayings = ['Fortune favors the brave.',
+'A joke is a very serious thing.',
+'Where there is love there is life.'];
+
+const listItem = (famousSayings[0]);
+console.log(listItem);
+//Prints: Fortune favors the brave.//(index[0])
+
+console.log(famousSayings[2]);
+//Prints: Where there is love there is life.//(index[2])
+
+console.log(famousSayings[3]);
+//Prints: undefined // because we do not have item with index[3]
