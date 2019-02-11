deead17c9009daf2d28cedecf521925b366946fa
IerizhenkoGalina <44175437+IerizhenkoGalina@users.noreply.github.com>
2019-02-11 14:20:23 +0200

    Create numbers.js
    (second way to created arrays)
    Arrays are JavaScript's way of making lists. Arrays can store any data types, including numbers. Like lists, arrays are ordered, meaning each item has a numbered position.

---
+++ list.js/numbers.js
@@ -0,0 +1,12 @@
+let accountListed = [];
+accountListed[0]='3788-11';
+accountListed[1]='3788-12';
+accountListed[2]='3788-13';
+accountListed[3]='3788-14';
+accountListed[4]='3788-15';
+accountListed[5]='3788-16';
+console.log(accountListed);
+//Prints:
+/*
+[ '3788-11', '3788-12', '3788-13', '3788-14', '3788-15', '3788-16' ]
+*/
