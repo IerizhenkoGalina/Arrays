4436b2c47f02655e1d3a2797f8ef019c725dc056
IerizhenkoGalina <44175437+IerizhenkoGalina@users.noreply.github.com>
2019-02-11 15:32:33 +0200

    Create the .length property.js
    When we want to know how many elements are in an array, we can access the .length property.

---
+++ the .length property.js
@@ -0,0 +1,6 @@
+const roomPlaces = ['garage', 'balcony', 'hall',
+'kitchen', 'dinning room', 'living room',
+'stairs', 'home office', 'bedroom',
+'roof tarrace', 'garden'];
+console.log(roomPlaces.length);
+//Prints: 11
