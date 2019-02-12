cec767b13b2c732a8ece05508a5d627d402b8a33
IerizhenkoGalina <44175437+IerizhenkoGalina@users.noreply.github.com>
2019-02-12 16:40:13 +0200

    Create Nested.js
     When an array contains another array it is known as a nested array.

---
+++ Nested.js
@@ -0,0 +1,5 @@
+const numberClusters = [[1,2],[3,4],[5,6]];
+const target = numberClusters[2][1];// nested array = index[2] of first array, index[1]=element with the nested array
+console.log(target);
+//Prints: 6
+
