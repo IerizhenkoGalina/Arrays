ddfaeaedfa67a43cc56d3e217db40f205ec93aed
IerizhenkoGalina <44175437+IerizhenkoGalina@users.noreply.github.com>
2019-02-12 12:18:49 +0200

    Create the .pop().js
    This array method, .pop(), removes the last item of an array.

---
+++ Methods.js/the .pop().js
@@ -0,0 +1,13 @@
+const chooseDay = ['Monday', 'Tuesday', 'Wednesday',
+'Thursday', 'Friday', 'Saturday', 'Sunday'];
+chooseDay.pop();
+console.log(chooseDay);
+//Prints:
+/*
+[ 'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday' ]
+*/
