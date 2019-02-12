9606db62057cc6b1477730b08a1e0e940a6c34e6
IerizhenkoGalina <44175437+IerizhenkoGalina@users.noreply.github.com>
2019-02-12 13:59:10 +0200

    Create the .shift().js
    The shift() method removes the first element from an array and returns that removed element. This method changes the length of the array.

---
+++ Methods.js/the .shift().js
@@ -0,0 +1,14 @@
+const groceryList = ['orange juice', 'bananas',
+'coffee beans', 'brown rice', 'pasta',
+'coconut oil', 'plantains'];
+groceryList.shift(); // removes the first element
+console.log(groceryList);
+//Prints:
+/*
+[ 'bananas',
+  'coffee beans',
+  'brown rice',
+  'pasta',
+  'coconut oil',
+  'plantains' ]
+*/
