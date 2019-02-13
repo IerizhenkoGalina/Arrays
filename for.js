7b3ced02d8cab931ab30d65fdfef4a2fb5df3b14
IerizhenkoGalina <44175437+IerizhenkoGalina@users.noreply.github.com>
2019-02-13 15:59:53 +0200

    Create for.js

---
+++ for.js
@@ -0,0 +1,22 @@
+const vacationSpots = ['Bali', 'Paris', 'Tulum'];
+for(let i = 0; i< vacationSpots.length; i++ ) {
+  console.log('I would love to visit ' + vacationSpots[i]);
+};
+//Prints:
+/*
+I would love to visit Bali
+I would love to visit Paris
+I would love to visit Tulum
+*/
+
+const vacationSpot = ['Bali', 'Paris', 'Tulum'];
+for(let i = 2; i< vacationSpot.length; i++ ) {
+  console.log('I would love to visit ' + vacationSpot[i]);
+};
+//Prints: I would love to visit Tulum
+
+const vacationSpo = ['Bali', 'Paris', 'Tulum'];
+for(let i = 2; i< vacationSpo.length; i++ ) {
+  console.log(vacationSpo[i]);
+};
+//Prints: Tulum
